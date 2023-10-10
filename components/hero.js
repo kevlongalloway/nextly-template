5177af1c7c91ddf31afd174c35d01f82997a7321
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-10-10 14:24:07 -0700

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -9,10 +9,10 @@ const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Welcome to PocketGuru: Your Personalized Mindfulness and Meditation Companion
+             Welcome to PocketGuru: Your Personalized Mindfulness Companion
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Rediscover a profound sense of serenity as you embark on a transformative journey with PocketGuru. Our app is meticulously designed to help you reduce stress, find balance, and cultivate a lasting sense of well-being.
+              Experience a tailored path to inner peace. PocketGuru guides you towards balance, reducing stress for a harmonious life.
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
